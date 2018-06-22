4fd5f97dbcba7965db765464f6128980ad6323e5
Animesh <animeshmane@gmail.com>
2018-06-22 17:25:57 -0400

    Login

--- src/main/java/myapp/repositories/UserRepository.java
+++ src/main/java/myapp/repositories/UserRepository.java
@@ -1,7 +1,15 @@
 package myapp.repositories;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import myapp.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	@Query("Select p from User p where p.email =:email and p.password =:password")
+	public List<User>findUserByUsernameAndPassword(@Param("email") String email,@Param("password") String password);
 
 }
--- src/main/java/myapp/services/UserService.java
+++ src/main/java/myapp/services/UserService.java
@@ -1,6 +1,11 @@
 package myapp.services;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,29 @@ public class UserService {
 		return userRepository.save(user);
 	}
 	
+	@GetMapping("/api/user")
+	public Iterable<User> findAllUser() {
+		return userRepository.findAll();
+	}
 	
+	@PostMapping("/api/login")
+	public User login(@RequestBody User user) {
+		System.out.println(user);
+		System.out.println("Inside login");
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+	
+		List<User> usersList = userRepository.findUserByUsernameAndPassword(user.getEmail(),user.getPassword());
+		if(usersList.size() == 0) {
+			user =  new User();
+			System.out.println(user.getId());
+		}
+		
+		else {
+			user = usersList.get(0);
+			System.out.println("Here I am :"+user.getId());
+		}
+		return user;
+	}
 
 }
