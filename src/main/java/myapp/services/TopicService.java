d99f6ac4f57a991cc53512c5b0bd7c91afcd5c27
Animesh <animeshmane@gmail.com>
2018-06-23 16:04:12 -0400

    delete top picks

--- src/main/java/myapp/services/TopicService.java
+++ src/main/java/myapp/services/TopicService.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,11 @@ public Topic createTopic(@RequestBody Topic topic1) {
 public Iterable<Topic>findAllTopics(){
 	return topicRepository.findAll();
 	
+}
+
+@DeleteMapping("/api/topic/{topicId}")
+public void delete(@PathVariable("topicId") int id) {
+	topicRepository.deleteById(id);	
 }
 
 
