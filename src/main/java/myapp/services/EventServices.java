1eefbd28d9516f0203fd8cdb1ee7bd5cec1a72e3
shravyaraj <38574502+shravyaraj@users.noreply.github.com>
2018-06-25 00:50:19 -0400

    heroku deploy test

--- src/main/java/myapp/services/EventServices.java
+++ src/main/java/myapp/services/EventServices.java
@@ -17,10 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import myapp.models.Blog;
 import myapp.models.Event;
-import myapp.models.User;
+
 import myapp.repositories.EventRepository;
 
 @RestController
