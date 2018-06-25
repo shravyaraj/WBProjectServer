c70f0a72db566a441293f374869a26469998f778
Animesh <animeshmane@gmail.com>
2018-06-24 20:48:21 -0400

    heroku deploy

--- src/main/java/myapp/services/BlogService.java
+++ src/main/java/myapp/services/BlogService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,11 @@ public class BlogService {
 		
 	}
 	
+	@DeleteMapping("/api/blog/{blogId}")
+	public void delete(@PathVariable("topicId") int id) {
+		blogRepository.deleteById(id);	
+	}
+
 	
 	
 
