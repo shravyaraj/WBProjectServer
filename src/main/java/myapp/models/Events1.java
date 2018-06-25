6f3a2747ae839d6d8d45b59685d7280b2ebf073f
shravyaraj <38574502+shravyaraj@users.noreply.github.com>
2018-06-25 19:16:40 -0400

    heroku deploy

---
+++ src/main/java/myapp/models/Events1.java
@@ -0,0 +1,49 @@
+package myapp.models;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+@Table(name = "events1")
+public class Events1 {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public Date getModified() {
+		return modified;
+	}
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date modified;
+
+}
