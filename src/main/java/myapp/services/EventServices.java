cb7a0107da9b76d40a2aaee601684ac44b04e048
shravyaraj <38574502+shravyaraj@users.noreply.github.com>
2018-06-25 00:07:17 -0400

    changes

--- src/main/java/myapp/services/EventServices.java
+++ src/main/java/myapp/services/EventServices.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import myapp.models.Event;
-import myapp.models.User;
+
 import myapp.repositories.EventRepository;
 
 @RestController
